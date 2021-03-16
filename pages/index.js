79d9eb0167c8ea766d4e881d03efb7650c165c95
Anthony Young <ayoung93@gmail.com>
2021-03-16 19:32:17 +0000

    add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -3,51 +3,61 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="md:container md:mx-auto">
       <Head>
         <title>Anthony Young</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.grid}>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="flex justify-center">
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </div>
+
+          <div className="flex justify-center">
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h3>Learn &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+          </div>
+
+          <div className="flex justify-center">
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className={styles.card}
+            >
+              <h3>Examples &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+          </div>
+
+          <div className="flex justify-center">
+            <a
+              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h3>Deploy &rarr;</h3>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Anthony Young
-        </h1>
+        <h1 className={styles.title}>Anthony Young</h1>
 
         <p className={styles.description}>
-          New site coming soon{' '}
+          New site coming soon{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div className={styles.grid}></div>
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +66,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Proudly Powered by{' '}
+          Proudly Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
