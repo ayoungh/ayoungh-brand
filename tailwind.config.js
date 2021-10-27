afb60798efa30aa3fa63caf107df2d664d0f095f
Anthony Young <404328+ayoungh@users.noreply.github.com>
2021-10-27 12:10:11 +0100

    packages and typogrphay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       gridTemplateColumns: {
-        main: "repeat(auto-fit, minmax(320px, 1fr))",
+        main: 'repeat(auto-fit, minmax(320px, 1fr))',
 
         // Complex site-specific column configuration
-        footer: "200px minmax(900px, 1fr) 100px",
+        footer: '200px minmax(900px, 1fr) 100px',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
