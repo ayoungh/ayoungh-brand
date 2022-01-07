acb36ab9a13bdfe2dc2382a5ec8c8c6401046d3e
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-01-07 21:35:05 +0000

    clean up config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        main: 'repeat(auto-fit, minmax(320px, 1fr))',
-
-        // Complex site-specific column configuration
-        footer: '200px minmax(900px, 1fr) 100px',
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
 };
